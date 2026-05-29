0736bd95623ebe197c1373ab00d1a976809f4a48
Josué Santos Silva <josuesasilva@gmail.com>
2026-05-29 11:33:01 -0300

    add typescript lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,113 +1,158 @@
 return {
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "mason-org/mason.nvim" },
-		opts = {
-			ensure_installed = { "lua_ls", "rust_analyzer" },
-			automatic_installation = true,
-			automatic_enable = false,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			vim.diagnostic.config({
-				signs = true,
-				underline = true,
-				severity_sort = true,
-				virtual_text = {
-					spacing = 2,
-					source = "if_many",
-					prefix = "●",
-				},
-				float = {
-					border = "rounded",
-					source = "if_many",
-				},
-			})
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls", "rust_analyzer" },
+      automatic_installation = true,
+      automatic_enable = false,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      ---
+      --- General LSP configs
+      ---
 
-			vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+      vim.diagnostic.config({
+        signs = true,
+        underline = true,
+        severity_sort = true,
+        virtual_text = {
+          spacing = 2,
+          source = "if_many",
+          prefix = "●",
+        },
+        float = {
+          border = "rounded",
+          source = "if_many",
+        },
+      })
 
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			local rust_check_command = vim.fn.executable("cargo-clippy") == 1 and "clippy" or "check"
+      vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
-			local function custom_on_attach(_, bufnr)
-				vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-				local opts = { buffer = bufnr, noremap = true, silent = true }
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-			end
+      local function custom_on_attach(_, bufnr)
+        vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-			local sourcekit_cmd = vim.env.SOURCEKIT_LSP_PATH and { vim.env.SOURCEKIT_LSP_PATH } or { "sourcekit-lsp" }
+        --- Define keymaps
+        local opts = { buffer = bufnr, noremap = true, silent = true }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-			vim.lsp.config("sourcekit", {
-				cmd = sourcekit_cmd,
-				filetypes = { "swift", "objc", "objcpp", "c", "cpp" },
-				root_markers = { ".bsp", "Package.swift", "compile_commands.json", ".git" },
-				on_attach = custom_on_attach,
-				capabilities = capabilities,
-			})
+        --- Enable formatting on save
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end
 
-			vim.lsp.config("lua_ls", {
-				on_attach = custom_on_attach,
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-						workspace = { checkThirdParty = false },
-						telemetry = { enable = false },
-					},
-				},
-			})
+      ---
+      --- Swift LSP configs
+      ---
 
-			vim.lsp.config("rust_analyzer", {
-				on_attach = custom_on_attach,
-				capabilities = capabilities,
-				settings = {
-					["rust-analyzer"] = {
-						cargo = {
-							allFeatures = true,
-							loadOutDirsFromCheck = true,
-							buildScripts = {
-								enable = true,
-							},
-						},
-						checkOnSave = true,
-						check = {
-							command = rust_check_command,
-						},
-						diagnostics = {
-							enable = true,
-						},
-						procMacro = {
-							enable = true,
-						},
-						files = {
-							exclude = {
-								".direnv",
-								".git",
-								".jj",
-								".github",
-								".gitlab",
-								"bin",
-								"node_modules",
-								"target",
-								"venv",
-								".venv",
-							},
-							watcher = "client",
-						},
-					},
-				},
-			})
+      local sourcekit_cmd = vim.env.SOURCEKIT_LSP_PATH and { vim.env.SOURCEKIT_LSP_PATH } or { "sourcekit-lsp" }
 
-			vim.lsp.enable({ "sourcekit", "lua_ls", "rust_analyzer" })
-		end,
-	},
+      vim.lsp.config("sourcekit", {
+        cmd = sourcekit_cmd,
+        filetypes = { "swift", "objc", "objcpp", "c", "cpp" },
+        root_markers = { ".bsp", "Package.swift", "compile_commands.json", ".git" },
+        on_attach = custom_on_attach,
+        capabilities = capabilities,
+      })
+
+      ---
+      --- Lua LSP configs
+      ---
+
+      vim.lsp.config("lua_ls", {
+        on_attach = custom_on_attach,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+          },
+        },
+      })
+
+      ---
+      --- Rust LSP configs
+      ---
+
+      local rust_check_command = vim.fn.executable("cargo-clippy") == 1 and "clippy" or "check"
+
+      vim.lsp.config("rust_analyzer", {
+        on_attach = custom_on_attach,
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+            },
+            checkOnSave = true,
+            check = {
+              command = rust_check_command,
+            },
+            diagnostics = {
+              enable = true,
+            },
+            procMacro = {
+              enable = true,
+            },
+            files = {
+              exclude = {
+                ".direnv",
+                ".git",
+                ".jj",
+                ".github",
+                ".gitlab",
+                "bin",
+                "node_modules",
+                "target",
+                "venv",
+                ".venv",
+              },
+              watcher = "client",
+            },
+          },
+        },
+      })
+
+      ---
+      --- Typescript LSP configs
+      ---
+      vim.lsp.config("ts_ls", {
+        on_attach = custom_on_attach,
+        capabilities = vim.lsp.protocol.make_client_capabilities(),
+        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayFunctionLikeReturnTypes = true,
+            },
+          },
+        },
+      })
+
+      vim.lsp.enable({ "sourcekit", "lua_ls", "rust_analyzer", "ts_ls" })
+    end,
+  },
 }
